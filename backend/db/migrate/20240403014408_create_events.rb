39e786cdbd72949fb5685ef9561708acdf8540ff
K-Katsumata9 <dowbler@icloud.com>
2024-04-03 15:44:43 +0900

    add Rails Project

---
+++ backend/db/migrate/20240403014408_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration[7.1]
+
+  def change
+    create_table :events do |t|
+      t.string :name, limit: 100, null: false
+      t.datetime :start, null: false   
+      t.datetime :end, null: false  
+      t.boolean :timed, default: true
+      t.text :description
+      t.string :color
+
+      t.timestamps
+    end
+  end
+   
+end
